1250442c2a547a68e69ccacb32777b3d65643dab
Zexin Chen <545447206@qq.com>
2024-11-27 12:37:32 +0800

    Update  gaoxiaojingsaiguanlixitongApplication.java

--- back/src/main/java/com/gaoxiaojingsaiguanlixitongApplication.java
+++ back/src/main/java/com/gaoxiaojingsaiguanlixitongApplication.java
@@ -15,9 +15,12 @@ public class gaoxiaojingsaiguanlixitongApplication extends SpringBootServletInit
 	public static void main(String[] args) {
 		SpringApplication.run(gaoxiaojingsaiguanlixitongApplication.class, args);
 	}
-	
+
+
+	// 该方法用于支持在传统的Servlet容器中运行Spring Boot应用，提供war包的支持
 	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
-        return applicationBuilder.sources(gaoxiaojingsaiguanlixitongApplication.class);
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		// 返回配置Spring Boot应用的Builder，指定主类gaoxiaojingsaiguanlixitongApplication作为应用的入口
+		return applicationBuilder.sources(gaoxiaojingsaiguanlixitongApplication.class);
     }
 }
